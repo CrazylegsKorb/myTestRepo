5e0322ea1b1674674247a72cc805c73ade7de30e
Spencer <spencerkorb@gmail.com>
2022-04-06 14:02:25 -0400

    Updated abstract animal class

--- animal.java
+++ animal.java
@@ -2,4 +2,17 @@ abstract class Animal {
     String name;
     String sound;
     int size;
+
+    //Sound accessors
+    abstract String speak();
+    abstract void setSound(String sound);
+
+    //Name accessors 
+    abstract String getName();
+    abstract void setName(String name);
+
+    //Size accessors
+    abstract int getSize();
+    abstract void setSize(int num);
+
 }
